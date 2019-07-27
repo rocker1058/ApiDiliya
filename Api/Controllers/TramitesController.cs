7b73dacb3e38032d6e06d1421463e0aea210938f
Leonardo Arango <rocker1058@gmail.com>
2019-07-27 17:28:18 -0500

    Api

--- Api/Controllers/TramitesController.cs
+++ Api/Controllers/TramitesController.cs
@@ -30,19 +30,19 @@ namespace ServicioApiTramites.Controllers
 
         {
 
-            Tramite p = new Tramite { IdTramite = 1, Nombre = «Lucia», Email = «lucia@gmail.com», Edad = 19 };
+            Tramite p = new Tramite { IdTramite = 1, Nombre = «Lucia», Email = «lucia@gmail.com», Cedula = 19111 };
 
             this.listaTramites.Add(p);
 
-            p = new Tramite { IdTramite = 2, Nombre = «Adrian», Email = «adrian@gmail.com», Edad = 24 };
+            p = new Tramite { IdTramite = 2, Nombre = «Adrian», Email = «adrian@gmail.com», Cedula = 24111 };
 
             this.listaTramites.Add(p);
 
-            p = new Tramite { IdTramite = 3, Nombre = «Alejandro», Email = «alejandro@gmail.com», Edad = 21 };
+            p = new Tramite { IdTramite = 3, Nombre = «Alejandro», Email = «alejandro@gmail.com», Cedula = 21111 };
 
             this.listaTramites.Add(p);
 
-            p = new Tramite { IdTramite = 4, Nombre = «Sara», Email = «sara@gmail.com», Edad = 17 };
+            p = new Tramite { IdTramite = 4, Nombre = «Sara», Email = «sara@gmail.com», Cedula = 17111 };
 
             this.listaTramites.Add(p);
 
